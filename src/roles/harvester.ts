247942d9651e5fee5041eedcb9faf78e1d1fa0b9
bastianerler <erler.bastian@gmail.com>
2019-10-26 11:30:51 +0200

    bugfix in harvester code

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -1,11 +1,6 @@
 export default {
   run: (creep: MyCreep) => {
-    if (creep.carry.energy === 0) {
-      const sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
-      }
-    } else {
+    if (creep.memory.working) {
       const targets = creep.room.find(FIND_MY_STRUCTURES, {
         filter: (structure: AnyStructure) => {
           return (structure.structureType === STRUCTURE_EXTENSION ||
@@ -18,6 +13,13 @@ export default {
           creep.moveTo(targets[0], { visualizePathStyle: { stroke: "#ffffff" } });
         }
       }
+      creep.memory.working = creep.carry.energy > 0;
+    } else {
+      const sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+      }
+      creep.memory.working = (creep.carry.energy === creep.carryCapacity);
     }
   }
 };
