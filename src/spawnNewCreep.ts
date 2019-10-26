fae1c133d95c9a431733e03cf0a241ef30f7bb89
bastianerler <erler.bastian@gmail.com>
2019-10-26 22:16:55 +0200

    added new spawnCreeps logic

--- src/spawnNewCreep.ts
+++ src/spawnNewCreep.ts
@@ -1,9 +1,28 @@
-import creepsBlueprints from "./config/creepsBlueprints";
+const getBluePrint = (energyAvailable: number): BodyPartConstant[] => {
+  const workPartsCount = Math.round(energyAvailable * 0.5  / 100);
+  const energyAvailableAfterWorkParts = energyAvailable - workPartsCount * 100;
+  const carryPartsCount = Math.round(energyAvailableAfterWorkParts * 1.2 / 100);
+  const energyAvailableAfterCarryParts = energyAvailableAfterWorkParts - carryPartsCount * 50;
+  const moveParts = Math.floor(energyAvailableAfterCarryParts / 50);
+  const bodyParts: BodyPartConstant[] = [];
+  console.log(workPartsCount, carryPartsCount, moveParts);
+  pushPartTimes(bodyParts, WORK, workPartsCount);
+  pushPartTimes(bodyParts, CARRY, carryPartsCount);
+  pushPartTimes(bodyParts, MOVE, moveParts);
+  console.log(bodyParts);
+  return bodyParts;
+};
+
+const pushPartTimes = (bodyParts: BodyPartConstant[], part: BodyPartConstant, times: number) => {
+  for (let i = 0; i < times; i++) {
+    bodyParts.push(part);
+  }
+};
 
 export default (role: string, roomName: string) => {
   for (const spawn in Game.spawns) {
     const result = Game.spawns[spawn].spawnCreep(
-      creepsBlueprints.getBluePrint(
+      getBluePrint(
         Game.rooms[roomName].energyCapacityAvailable
       ),
       role + Math.round(Math.random() * 1000),
