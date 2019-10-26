52fe7384cfb44582d190fe22e14d363c42dd20e9
bastianerler <erler.bastian@gmail.com>
2019-10-26 11:19:34 +0200

    small change in harvester code to empty capacity before refilling

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -1,6 +1,6 @@
 export default {
   run: (creep: MyCreep) => {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.carry.energy === 0) {
       const sources = creep.room.find(FIND_SOURCES);
       if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
         creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
